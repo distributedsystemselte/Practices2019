e1c9b2d68f89f64cd1fb8ec5e0630a3826550f9d
dulguun <dulguunb@protonmail.com>
2019-03-10 21:08:26 +0100

    chat application updated

--- 20190228/ChatApplication/Client.java
+++ 20190228/ChatApplication/Client.java
@@ -1,38 +1,46 @@
-import java.util.*;
-import java.io.*;
-import java.net.*;
-
-class Client
-{
-    public static void main(String[] args)
-        throws Exception
-    {
-        int port = 12345;
-        String computer = "localhost";
-        Scanner commandLine = new Scanner(System.in);
-        try (
-            Socket s = new Socket(computer, port);
-
-            Scanner sc = new Scanner(s.getInputStream());
-            PrintWriter pw = new PrintWriter(s.getOutputStream());
-        ) {
-            pw.println(args[0]);
-            pw.flush();
-            int init = sc.nextInt();
-            System.out.println("Init="+init);
-            if(init == 1){
-                System.out.println("CONNECTED!");
-                while(true){
-                    System.out.println("Your msgs: ");
-                    String  msgs = commandLine.nextLine();
-                    String receivedMsg = sc.nextLine();
-                    pw.println(msgs);
-                    pw.flush();
-                    System.out.println("Received Msg: " + receivedMsg);
-
-                }
-            }
-        }
-
-    }
-}
+import java.io.*; 
+import java.net.*; 
+import java.util.Scanner; 
+// instruction run this with : java Client user_1 -> means first 0th client wants to talk to 1st client
+// second client should connect to the first one by writing java Client user_0
+public class Client  
+{ 
+    final static int ServerPort = 1234; 
+    public static void main(String args[]) throws UnknownHostException, IOException  
+    { 
+        Scanner scn = new Scanner(System.in);           
+        Socket s = new Socket("localhost", ServerPort);           
+        Scanner dis = new Scanner(s.getInputStream()); 
+        PrintWriter dos = new PrintWriter(s.getOutputStream()); 
+        String name = args[0];
+        Thread sendMessage = new Thread(
+	        new Runnable()  
+	        { 
+	            @Override
+	            public void run() { 
+	                while (true) { 
+	                	String msg = scn.nextLine(); 
+	                	String response=msg+"#"+name;
+	                    dos.println(response); 
+	                    dos.flush();
+	                } 
+	            } 
+	        }
+        ); 
+        Thread readMessage = new Thread(new Runnable()  
+        { 
+            @Override
+            public void run() { 
+  
+                while (true) { 
+                    String msg = dis.nextLine(); 
+                    System.out.println(msg); 
+                } 
+            } 
+        }); 
+  
+        sendMessage.start(); 
+        readMessage.start(); 
+  
+    } 
+} 
--- 20190228/ChatApplication/Server.java
+++ 20190228/ChatApplication/Server.java
@@ -1,62 +1,26 @@
-import java.util.*;
-import java.io.*;
-import java.net.*;
-import Utils.User;
-
-public class Server{
-        public static void main(String[] args){
-        int PORT = 12345;
-        String computer = "localhost";
-        ServerSocket serverSocket = null;
-        PrintWriter printToUsers = null;
-        Scanner scanner = null;
-        Socket socket = null;
-        Object obj = new Object();
-
-        try {
-            serverSocket = new ServerSocket(PORT);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        ArrayList<User> users = new ArrayList<>();
-        int n=0;
-        while (true) {
-            try {
-                socket = serverSocket.accept();
-                printToUsers = new PrintWriter(socket.getOutputStream());
-                scanner = new Scanner(socket.getInputStream());
-            } catch (IOException e) {
-                System.out.println("I/O error: " + e);
-            }
-            try{
-            	n++;
-            	System.out.println("Connected users= "+n);
-            	User user1 = new User(socket,obj,scanner,printToUsers);
-            	users.add(user1);
-		        user1.start();
-	        }            
-            catch (Exception e){
-            	System.out.println(e);
-            }
-            if(n == 2){
-            	System.out.println("init the msgs = " + n);
-            	users.get(0).getPrintWriter().println(1);
-            	users.get(0).getPrintWriter().flush();
-            	users.get(1).getPrintWriter().println(1);
-            	users.get(1).getPrintWriter().flush();
-            	String name0 = users.get(0).getUserName();
-            	String name1 = users.get(1).getUserName();
-            	String user0Text = users.get(0).getScanner().nextLine();
-            	String user1Text = users.get(1).getScanner().nextLine();
-            	users.get(0).getPrintWriter().println(user1Text);
-            	users.get(0).getPrintWriter().flush();
-            	users.get(1).getPrintWriter().println(user0Text);
-            	users.get(1).getPrintWriter().flush();
-            	System.out.println(name0+ " says: " + user0Text);
-            	System.out.println(name1+ " says: " + user1Text);
-            }
-        }
-        	
-	}
- }
-
+import java.io.*; 
+import java.util.*; 
+import java.net.*; 
+public class Server  
+{ 
+    static Vector<ChatClient> ar = new Vector<>(); 
+    static int i = 0;  
+    public static void main(String[] args) throws Exception  
+    { 
+        ServerSocket ss = new ServerSocket(1234);         
+        Socket s; 
+        while (true)  
+        { 
+            s = ss.accept();   
+            Scanner dis = new Scanner(s.getInputStream()); 
+            PrintWriter dos = new PrintWriter(s.getOutputStream());   
+  		    ChatClient mtch = new ChatClient(s,"user_" + i, dis, dos); 
+            Thread t = new Thread(mtch); 
+            System.out.println("Adding this client to active client list"); 
+            ar.add(mtch);
+            t.start(); 
+            i++; 
+  
+        } 
+    } 
+} 
